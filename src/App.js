dbc9d0babac6aaefb4eb87dca2d41e2ee25e5056
belalQassem <belalqassem52@gmail.com>
2023-09-11 11:47:36 +0300

    changes and split code

--- src/App.js
+++ src/App.js
@@ -54,63 +54,37 @@
 
 // export default App;
 
-import React, { lazy, Suspense } from "react";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+// App.js
+
+import {React,Suspense} from "react";
+import { BrowserRouter } from "react-router-dom";
 import { useAuthContext } from "./context/AuthContext";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./utils/theme";
-import Layout from "./components/LayOut/LayOut";
 import Loading from "./components/Loading";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Router from './routes/Router'; 
 
-
-// Lazy-loaded pages
-const LazyLogin = lazy(() => import("./pages/Login/Login"));
-const LazyDashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
-const LazyActivatedKeys = lazy(() => import("./pages/Keys/AktivatedKeys"));
-const LazypendingdKeys = lazy(() => import("./pages/Keys/PendingKeys"));
-
-const LazyMangeTeam= lazy(() => import("./pages/MangeTeam/MangeTeam"));
-const LazyCreateUser = lazy(() => import("./pages/CreateUser/CreateUser"));
-const LazyUpdateUser = lazy(() => import("./pages/UpdateUser/UpdateUser"));
-
-
-
-
-function App() {
+const App=() => {
   const { isAuthorized } = useAuthContext();
   const [theme, colorMode] = useMode();
+  
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-    <div className="App">
-      <Suspense fallback={<Loading />}>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={isAuthorized === true ? <Layout/> : <Navigate to="/login" />}
-            >
-              <Route path="/" element={<LazyDashboard />} />
-              <Route path="/dashboard/Keys/activated" element={<LazyActivatedKeys />} />
-              <Route path="/dashboard/Keys/pending" element={<LazypendingdKeys />} />
-              <Route path="/dashboard/data/usermangment" element={<LazyMangeTeam />} />
-              <Route path="/dashboard/data/create" element={<LazyCreateUser />} />
-              <Route path="/dashboard/update" element={<LazyUpdateUser />} />
-            </Route>
-            <Route path="/login" element={<LazyLogin />} />
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-    </div>
-    </ThemeProvider>
+        <div className="App">
+        <Suspense fallback={<Loading />}>
+          <BrowserRouter>
+            <Router isAuthorized={isAuthorized} />
+          </BrowserRouter>
+          </Suspense>  
+        </div>
+        <ToastContainer />
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
-
 export default App;
+
