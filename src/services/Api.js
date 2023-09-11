9e205dfa8cc2517970b9f06555bc4a8b393d6d37
belalQassem <belalqassem52@gmail.com>
2023-09-11 12:00:31 +0300

    initialize api functions

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,19 +2,32 @@ import axios from "axios";
 
 export const getallUsers = async (id) => {
     id = id || '';
-    return await axios.get(`${process.env.REACT_APP_API_URL}/${id}`);
+    return await axios.get(`http://185.196.21.227:3530/users/${id}`);
 }
 
-export const addUser = async (user) => {
-    return await axios.post(process.env.REACT_APP_API_URL,user);
+export const addUser = async (user,token) => {
+    return await axios.post(`${process.env.REACT_APP_API_URL}/users/insert`,user,{
+        headers: {
+          Authorization:`token ${token}`,
+        },
+      });
 }
 
-export const editUser = async (id, user) => {
-    return await axios.put(`${process.env.REACT_APP_API_URL}/${id}`,user);
+export const updateUser = async (id, user,token) => {
+    return await axios.put(`${process.env.REACT_APP_API_URL}/users/update/${id}`,user,{
+      headers: {
+        Authorization:`token ${token}`,
+      },
+    });
 }
-
-
-export const deleteUser = async (id) => {
-    return await axios.delete(`${process.env.REACT_APP_API_URL}/${id}`);
+export const deleteUser = async (id,token) => {
+    return await axios.delete(`${process.env.REACT_APP_API_URL}/users/delete/${id}`,{
+        headers: {
+          Authorization:`token ${token}`,
+        },
+      });
 }
   
+export const loginUser = async (email, password, otp) => {
+    return await axios.post(`${process.env.REACT_APP_API_URL}/login`,{ email, password,otp });
+ };
