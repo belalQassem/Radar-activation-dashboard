a2d6c3a5304a3f613a3cf6944d04ead2e2da64df
belalQassem <belalqassem52@gmail.com>
2023-09-11 11:59:57 +0300

    improve styles

--- src/pages/MangeTeam/style.js
+++ src/pages/MangeTeam/style.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import {BiEdit}from 'react-icons/bi'
+import {RiDeleteBin2Line}from 'react-icons/ri'
 
 export const StyledButton = styled.button`
   background-color: #ff0000;
@@ -8,8 +10,21 @@ export const StyledButton = styled.button`
   padding: 0.5rem 1rem;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
+  overflow-x: scroll;
 
   &:hover {
     background-color: #ff3333;
   }
 `;
+export const UpdateIcon = styled(BiEdit)`
+  color: #fff;
+  font-size: 1.5rem;
+  margin-right: 0.5rem;
+  cursor: pointer;
+`;
+export const DeleteIcon = styled(RiDeleteBin2Line)`
+  color: #fff;
+  font-size: 1.5rem;
+  margin-right: 0.5rem;
+  cursor: pointer;
+`
