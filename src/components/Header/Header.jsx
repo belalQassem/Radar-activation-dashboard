95e351f0d01f181dbbcbcc0f9e4ee4b36a6eb7ba
belalQassem <belalqassem52@gmail.com>
2023-09-17 23:51:28 +0300

    change header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,70 +1,14 @@
-// import { Box, IconButton, useTheme } from "@mui/material";
-// import { useContext } from "react";
-// import { ColorModeContext, tokens } from "../../utils/theme";
-// import InputBase from "@mui/material/InputBase";
-// import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-// import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-// import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-// import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-// import SearchIcon from "@mui/icons-material/Search";
-
-
-// const Header = () => {
-//   const theme = useTheme();
-//   const colors = tokens(theme.palette.mode);
-//   const colorMode = useContext(ColorModeContext);
-
-//   return (
-//     <Box display="flex" justifyContent="space-between" p={2}>
-//       {/* SEARCH BAR */}
-//       <Box
-//         display="flex"
-//         backgroundColor={colors.primary[400]}
-//         borderRadius="3px"
-//       >
-//         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" id="search" name="search" />
-//         <IconButton type="button" sx={{ p: 1 }}>
-//           <SearchIcon />
-//         </IconButton>
-//       </Box>
-
-//       {/* ICONS */}
-//       <Box display="flex">
-//         <IconButton onClick={colorMode.toggleColorMode}>
-//           {theme.palette.mode === "dark" ? (
-//             <DarkModeOutlinedIcon />
-//           ) : (
-//             <LightModeOutlinedIcon />
-//           )}
-//         </IconButton>
-//         <IconButton>
-//           <NotificationsOutlinedIcon />
-//         </IconButton>
-//         <IconButton>
-//           <SettingsOutlinedIcon />
-//         </IconButton>
-//         <IconButton>
-//           <PersonOutlinedIcon />
-//         </IconButton>
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default Header;
-
-import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { ColorModeContext, tokens } from "../../utils/theme";
 
 import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import SearchIcon from "@mui/icons-material/Search";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import NotificationsIcon from "@mui/icons-material/Notifications";
+// import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
 
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+// import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
 import { useContext } from "react";
 import { useProSidebar } from "react-pro-sidebar";
@@ -102,7 +46,7 @@ const Header = () => {
           </IconButton>
         )}
 
-        <Box
+        {/* <Box
           display="flex"
           backgroundColor={colors.primary[400]}
           borderRadius="3px"
@@ -111,7 +55,7 @@ const Header = () => {
           <IconButton type="button" sx={{ p: 1 }}>
             <SearchIcon />
           </IconButton>
-        </Box>
+        </Box> */}
       </Box>
 
       <Box
@@ -132,7 +76,7 @@ const Header = () => {
           )}
         </IconButton>
 
-        <IconButton type="button">
+        {/* <IconButton type="button">
           <NotificationsIcon />
         </IconButton>
 
@@ -142,7 +86,7 @@ const Header = () => {
 
         <IconButton type="button">
           <PersonOutlinedIcon />
-        </IconButton>
+        </IconButton> */}
       </Box>
     </Box>
   );
