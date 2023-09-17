418c72a614f511e6058b0e687047efe3a40af04a
belalQassem <belalqassem52@gmail.com>
2023-09-17 22:20:13 +0300

    change loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,14 +1,12 @@
 
 import React from "react";
 import { LoadingStyled } from "./style";
-// import Logo from "../../assets/logo-without-background-final-100x100-1.png"
+
 
 const Loading = () => {
     return (
         <>
-            {/* <img src={Logo} alt="logo" /> */}
             <LoadingStyled>
-
                 <div className="loading-spinner"></div>
                 <p className="loading-message">Loading...</p>
             </LoadingStyled>
