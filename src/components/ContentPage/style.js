d42606400b140934d9d182b7e3170654fc135867
belalQassem <belalqassem52@gmail.com>
2023-09-17 22:17:28 +0300

    change style

--- src/components/ContentPage/style.js
+++ src/components/ContentPage/style.js
@@ -1,13 +1,6 @@
 import styled from "styled-components";
 
 export const StyledDiv = styled.div`
-    position: absolute;
-    top: 0;
-    left: 20vw;
-    right: 0;
-    width: 78vw;
-    @media (max-width: 1200px) {
-         left : 0;
-         width: 100vw;
-     }
+    width: 100%;
+    height:72vh ;
 `;  
