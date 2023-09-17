9cb4d33c252a73dd31cc9c28133afea258733f79
belalQassem <belalqassem52@gmail.com>
2023-09-17 22:17:59 +0300

    modifide header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,56 +1,150 @@
-import { Box, IconButton, useTheme } from "@mui/material";
-import { useContext } from "react";
-import { ColorModeContext, tokens } from "../../utils/theme";
-import InputBase from "@mui/material/InputBase";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
+// import { Box, IconButton, useTheme } from "@mui/material";
+// import { useContext } from "react";
+// import { ColorModeContext, tokens } from "../../utils/theme";
+// import InputBase from "@mui/material/InputBase";
+// import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+// import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+// import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+// import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import SearchIcon from "@mui/icons-material/Search";
 
+
+// const Header = () => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   const colorMode = useContext(ColorModeContext);
+
+//   return (
+//     <Box display="flex" justifyContent="space-between" p={2}>
+//       {/* SEARCH BAR */}
+//       <Box
+//         display="flex"
+//         backgroundColor={colors.primary[400]}
+//         borderRadius="3px"
+//       >
+//         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" id="search" name="search" />
+//         <IconButton type="button" sx={{ p: 1 }}>
+//           <SearchIcon />
+//         </IconButton>
+//       </Box>
+
+//       {/* ICONS */}
+//       <Box display="flex">
+//         <IconButton onClick={colorMode.toggleColorMode}>
+//           {theme.palette.mode === "dark" ? (
+//             <DarkModeOutlinedIcon />
+//           ) : (
+//             <LightModeOutlinedIcon />
+//           )}
+//         </IconButton>
+//         <IconButton>
+//           <NotificationsOutlinedIcon />
+//         </IconButton>
+//         <IconButton>
+//           <SettingsOutlinedIcon />
+//         </IconButton>
+//         <IconButton>
+//           <PersonOutlinedIcon />
+//         </IconButton>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default Header;
+
+import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import { ColorModeContext, tokens } from "../../utils/theme";
+
+import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+import { useContext } from "react";
+import { useProSidebar } from "react-pro-sidebar";
 
 const Header = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+  const { broken, toggleSidebar } = useProSidebar();
 
   return (
-    <Box display="flex" justifyContent="space-between" p={2}>
-      {/* SEARCH BAR */}
+    <Box
+      display="flex"
+      flexWrap="wrap-reverse"
+      justifyContent="space-between"
+      p={2}
+    >
       <Box
         display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
+        justifyContent="space-between"
+        sx={{
+          [theme.breakpoints.down("sm")]: {
+            width: "100%",
+            justifyContent: "space-between",
+          },
+        }}
       >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
+        {broken && (
+          <IconButton
+            aria-label="Toggle Menu"
+            sx={{ marginRight: "10px", background: colors.primary[600] }}
+            onClick={() => toggleSidebar()}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+
+        <Box
+          display="flex"
+          backgroundColor={colors.primary[400]}
+          borderRadius="3px"
+        >
+          <InputBase sx={{ ml: 2, flex: 1 }} id="search" name="search" placeholder="Search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
       </Box>
 
-      {/* ICONS */}
-      <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
+      <Box
+        display="flex"
+        sx={{
+          [theme.breakpoints.down("sm")]: {
+            width: "100%",
+            justifyContent: "center",
+            marginBottom: "20px",
+          },
+        }}
+      >
+        <IconButton type="button" onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "light" ? (
+            <DarkModeOutlined />
           ) : (
-            <LightModeOutlinedIcon />
+            <LightModeOutlined />
           )}
         </IconButton>
-        <IconButton>
-          <NotificationsOutlinedIcon />
+
+        <IconButton type="button">
+          <NotificationsIcon />
         </IconButton>
-        <IconButton>
+
+        <IconButton type="button">
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
+
+        <IconButton type="button">
           <PersonOutlinedIcon />
         </IconButton>
       </Box>
     </Box>
   );
 };
-
 export default Header;
-
