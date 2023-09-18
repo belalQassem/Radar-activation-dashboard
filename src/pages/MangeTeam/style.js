0e8e095e805f95d72db79da7e7bff7c5e60f38f5
belalQassem <belalqassem52@gmail.com>
2023-09-18 11:45:05 +0300

    fixing style mange team

--- src/pages/MangeTeam/style.js
+++ src/pages/MangeTeam/style.js
@@ -2,20 +2,6 @@ import styled from "styled-components";
 import {BiEdit}from 'react-icons/bi'
 import {RiDeleteBin2Line}from 'react-icons/ri'
 
-export const StyledButton = styled.button`
-  background-color: #ff0000;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-  overflow-x: scroll;
-
-  &:hover {
-    background-color: #ff3333;
-  }
-`;
 export const UpdateIcon = styled(BiEdit)`
   color: #fff;
   font-size: 1.5rem;
